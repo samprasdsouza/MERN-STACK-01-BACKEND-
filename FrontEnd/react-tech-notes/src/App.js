67b987f832809ccaa7e38e3ed56d49a603a75f6b
Sampras Dsouza <samprasdsouza02@gmail.com>
2023-01-28 20:41:17 +0530

    bug fix

--- FrontEnd/react-tech-notes/src/App.js
+++ FrontEnd/react-tech-notes/src/App.js
@@ -9,23 +9,26 @@ import { UsersList } from './features/users/UsersList';
 
 function App() {
   return (
-    <>
-      <Routes>
-        <Route path='/' element={<Layout/>}> 
-          <Route index element={<Public/>} />
-          <Route path='login' element={<Login/>} />
-          <Route path='dash' element={<DashLayout/>} >
-            <Route index element={<Welcome/>}></Route>
-            <Route path='notes' >
-              <Route index element={<NotesList />} />
-            </Route>
-            <Route path='users'>
-              <Route index element={<UsersList/>} />
-            </Route>
-          </Route> {/* End Dash */}
+    <Routes>
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Public />} />
+      <Route path='login' element={<Login />} />
+
+      <Route path="dash" element={<DashLayout />}>
+
+        <Route index element={<Welcome />} />
+
+        <Route path="notes">
+          <Route index element={<NotesList />} />
         </Route>
-      </Routes>
-    </>
+
+        <Route path="users">
+          <Route index element={<UsersList />} />
+        </Route>
+
+      </Route>{/* End Dash */}
+    </Route>
+    </Routes>
   );
 }
 
